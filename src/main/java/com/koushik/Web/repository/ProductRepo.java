93d7e31f6ca52a33b2f4e90ee19ff8540c317351
KOUSHIK CHALLAGUNDLA <chk240404@gmail.com>
2026-02-01 22:18:14 +0530

    Added the image functionality

--- src/main/java/com/koushik/Web/repository/ProductRepo.java
+++ src/main/java/com/koushik/Web/repository/ProductRepo.java
@@ -3,6 +3,8 @@ package com.koushik.Web.repository;
 import com.koushik.Web.Model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepo extends JpaRepository<Product,Integer> {
+import java.util.List;
 
+public interface ProductRepo extends JpaRepository<Product,Integer> {
+//    List<Product> SearchProduct(String name);
 }
